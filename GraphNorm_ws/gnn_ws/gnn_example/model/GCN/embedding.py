3683c019b4270c827b8ad0836f57033f04e174c7
Verma Vikas <vermav1@t31300-lr150.org.aalto.fi>
2022-01-26 16:58:00 +0200

    minor

--- GraphNorm_ws/gnn_ws/gnn_example/model/GCN/embedding.py
+++ GraphNorm_ws/gnn_ws/gnn_example/model/GCN/embedding.py
@@ -3,7 +3,44 @@ import torch
 import numpy as np
 from model.Norm import *
  
-default_initializer = default_init
+def default_init(scale=1.):
+  """The same initialization used in DDPM."""
+  scale = 1e-10 if scale == 0 else scale
+  return variance_scaling(scale, 'fan_avg', 'uniform')
+
+
+def variance_scaling(scale, mode, distribution,
+                     in_axis=1, out_axis=0,
+                     dtype=torch.float32,
+                     device='cpu'):
+  """Ported from JAX. """
+
+  def _compute_fans(shape, in_axis=1, out_axis=0):
+    receptive_field_size = np.prod(shape) / shape[in_axis] / shape[out_axis]
+    fan_in = shape[in_axis] * receptive_field_size
+    fan_out = shape[out_axis] * receptive_field_size
+    return fan_in, fan_out
+
+  def init(shape, dtype=dtype, device=device):
+    fan_in, fan_out = _compute_fans(shape, in_axis, out_axis)
+    if mode == "fan_in":
+      denominator = fan_in
+    elif mode == "fan_out":
+      denominator = fan_out
+    elif mode == "fan_avg":
+      denominator = (fan_in + fan_out) / 2
+    else:
+      raise ValueError(
+        "invalid mode for variance scaling initializer: {}".format(mode))
+    variance = scale / denominator
+    if distribution == "normal":
+      return torch.randn(*shape, dtype=dtype, device=device) * np.sqrt(variance)
+    elif distribution == "uniform":
+      return (torch.rand(*shape, dtype=dtype, device=device) * 2. - 1.) * np.sqrt(3 * variance)
+    else:
+      raise ValueError("invalid distribution for variance scaling initializer")
+
+  return init
 
 class GaussianFourierProjectionTime(nn.Module):
   """Gaussian Fourier embeddings for noise levels."""
@@ -41,14 +78,14 @@ class Time_Embedding(nn.Module):
       modules.append(torch.nn.utils.spectral_norm(nn.Linear(nf_start, nf * 4)))
     else:
       modules.append(nn.Linear(nf_start, nf * 4))
-    modules[-1].weight.data = default_initializer()(modules[-1].weight.shape)
+    modules[-1].weight.data = default_init()(modules[-1].weight.shape)
     nn.init.zeros_(modules[-1].bias)
 
     if normalize_spectral:
       modules.append(torch.nn.utils.spectral_norm(nn.Linear(nf * 4, out_ch)))
     else:
       modules.append(nn.Linear(nf * 4, out_ch))
-    modules[-1].weight.data = default_initializer()(modules[-1].weight.shape)
+    modules[-1].weight.data = default_init()(modules[-1].weight.shape)
     nn.init.zeros_(modules[-1].bias)
 
     self.all_modules = nn.ModuleList(modules)
